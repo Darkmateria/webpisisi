b9e7551e350e4267e3d4481a039ea48fad729426
Darkmateria <darkmaterials491@gmail.com>
2024-07-28 12:30:19 -0500

    Primer commit: añadir todos los archivos del proyecto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@include('layouts.navbar')
+@extends('layouts.template')
+@include('layouts.headers')
+@include('layouts.gallery')
+@include('layouts.feature')
+@include('layouts.footer')
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::view('/', 'welcome')->name('home');
+
+Route::view('/nosotros', 'layouts.nosotros')->name('nosotros');
+Route::view('/caracteristicas', 'layouts.caracteristicas')->name('caracteristicas');
+Route::view('/beneficios', 'layouts.beneficios')->name('beneficios');
+Route::resource('files', FileController::class)->middleware('auth');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/logout', [ProfileController::class, 'logout'])->name('logout');
+
+});
+
+require __DIR__.'/auth.php';
